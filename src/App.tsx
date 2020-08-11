be215ee512196b4fa8b3ffe58101e6fb3f0d9b24
danielsampar12 <daniel_smapar@hotmail.com>
2020-08-11 14:31:13 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return <View />;
+};
+
+export default App;
